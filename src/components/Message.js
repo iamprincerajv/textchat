67aa59d72e1de83a5ba5c3e12951fea13abe0dd7
Prince <workwithprince2022@gmail.com>
2022-11-08 19:07:32 +0530

    improved messages

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
+import { useRef } from 'react';
 
 const TextMsg = (props) => {
-return <p className='textMsg p-2 ps-3 pe-4 mx-5 mt-1 rounded-3'>{props.msgVal}</p>
+    return <p className='textMsg p-2 ps-3 pe-4 mx-5 mt-1 rounded-3'>{props.msgVal}</p>
 }
 
 const Message = () => {
@@ -9,26 +10,32 @@ const Message = () => {
     const [msgVal, setMsgVal] = useState('');
     const [msg, setMsg] = useState([]);
 
+    const ref = useRef(null);
+
     const sendMsg = () => {
-        console.log('object');
-        setMsg(msg.concat(<TextMsg msgVal={msgVal} key={msg.length} />));
-        setMsgVal('');
+        if (msgVal.length > 0) {
+            setMsg(msg.concat(<TextMsg msgVal={msgVal} key={msg.length} />));
+            setMsgVal('');
+            ref.current.focus();
+        } else {
+            ref.current.focus()
+        }
     }
 
     return (
-        <div style={{ height: '91vh'}}>
+        <div style={{ height: '91vh' }}>
             <div className='messageBox'>
                 {msg}
             </div>
             <div className='send d-flex align-items-center justify-content-center'>
                 <form className='d-flex justify-content-center'>
-                    <input value={msgVal} type="text" placeholder='Enter Your Message Here' autoFocus onChange={(e)=>{setMsgVal(e.target.value)}} />
+                    <input ref={ref} value={msgVal} type="text" placeholder='Enter Your Message Here' autoFocus onChange={(e) => { setMsgVal(e.target.value) }} />
                     <div type="submit" onClick={sendMsg} className="sendBtn">
-                    <i className="fa-solid fa-paper-plane fa-2x"></i>
+                        <i className="fa-solid fa-paper-plane fa-2x"></i>
                     </div>
                 </form>
             </div>
-            </div>
+        </div>
     )
 }
 
