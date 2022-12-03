6f1727f5e127f12827d1574cac3fd8d12832c850
Prince <workwithprince2022@gmail.com>
2022-12-03 21:14:56 +0530

    changes in messaging UI

--- src/components/Message.js
+++ src/components/Message.js
@@ -73,40 +73,60 @@ const Message = () => {
 
     const showMore = async (id) => {
         let delBtn = document.getElementById(id);
-        if(delBtn) {
-            if(delBtn.style.display === "none") {
+        if (delBtn) {
+            if (delBtn.style.display === "none") {
                 delBtn.style.display = "block";
-            } else if(delBtn.style.display === "block") {
+            } else if (delBtn.style.display === "block") {
                 delBtn.style.display = "none";
             }
         }
     }
 
-return (
-    <div style={{ height: '91vh' }}>
-        <div className='messageBox'>
-            {
-                msgVal.length > 0 ? msgVal.map((items, index) => {
-                    return <div className=' mt-2 d-flex' key={items._id}>
-                        <p style={{ fontSize: '15px', width: "200px" }} className='ms-2 ms-lg-5 ms-sm-3 ms-md-4 ps-2 mt-1 pt-2'>{items.username}</p>
-                        <p onClick={()=>{showMore(items._id)}} className='textMsg p-2 ps-3 pe-3 mx-3 rounded-3'>{items.messageMe}</p>
-                        {
-                            items.username === localStorage.getItem("username") ? <i onClick={() => { deleteMsg(items._id) }} style={{ cursor: "pointer", display: "none" }} className={`pt-2 mt-1 fa-sharp fa-solid fa-trash`} id={`${items._id}`}></i> : ""
-                        }
+    return (
+        <div style={{ height: '91vh' }}>
+            <div className='messageBox'>
+                {
+                    msgVal.length > 0 ? msgVal.map((items, index) => {
+                        return <div className=' mt-2' key={items._id}>
+                            {
+                                items.username === localStorage.getItem("username") ? <div className='d-flex justify-content-start'>
+                                <p onClick={() => { showMore(items._id) }} className='textMsg p-2 ps-3 pe-3 mx-3 rounded-3'>
+                                    <p style={{ fontSize: '14px', color: "yellow", fontStyle: "italic" }} className='ps-1'>
+                                        {
+                                            items.username === localStorage.getItem("username") ? "You" : localStorage.getItem("friendToChatName")
+                                        }
+                                    </p>
+                                    {items.messageMe}</p>
+                                {
+                                    items.username === localStorage.getItem("username") ? <i onClick={() => { deleteMsg(items._id) }} style={{ cursor: "pointer", display: "none" }} className={`pt-2 mt-1 fa-sharp fa-solid fa-trash`} id={`${items._id}`}></i> : ""
+                                }
+                            </div> : <div className='d-flex justify-content-end'>
+                                <p onClick={() => { showMore(items._id) }} className='textMsg p-2 ps-3 pe-3 mx-3 rounded-3'>
+                                    <p style={{ fontSize: '14px', color: "yellow", fontStyle: "italic" }} className='ps-1'>
+                                        {
+                                            items.username === localStorage.getItem("username") ? "You" : localStorage.getItem("friendToChatName")
+                                        }
+                                    </p>
+                                    {items.messageMe}</p>
+                                {
+                                    items.username === localStorage.getItem("username") ? <i onClick={() => { deleteMsg(items._id) }} style={{ cursor: "pointer", display: "none" }} className={`pt-2 mt-1 fa-sharp fa-solid fa-trash`} id={`${items._id}`}></i> : ""
+                                }
+                            </div>
+                            }
+                        </div>
+                    }) : <p className='text-center'>No messages yet</p>
+                }
+            </div>
+            <div className='send d-flex align-items-center justify-content-center row'>
+                <form className='d-flex justify-content-center col-xl-4 col-lg-6 col-md-7 col-sm-8'>
+                    <input ref={ref} value={msg} type="text" placeholder='Enter Your Message Here' autoFocus onChange={(e) => { setMsg(e.target.value) }} />
+                    <div type="submit" onClick={sendMsg} className="sendBtn">
+                        <i className="fa-solid fa-paper-plane fa-2x"></i>
                     </div>
-                }) : <p className='text-center'>No messages yet</p>
-            }
+                </form>
+            </div>
         </div>
-        <div className='send d-flex align-items-center justify-content-center row'>
-            <form className='d-flex justify-content-center col-xl-4 col-lg-6 col-md-7 col-sm-8'>
-                <input ref={ref} value={msg} type="text" placeholder='Enter Your Message Here' autoFocus onChange={(e) => { setMsg(e.target.value) }} />
-                <div type="submit" onClick={sendMsg} className="sendBtn">
-                    <i className="fa-solid fa-paper-plane fa-2x"></i>
-                </div>
-            </form>
-        </div>
-    </div>
-)
+    )
 }
 
 export default Message
