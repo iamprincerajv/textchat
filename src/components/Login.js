f1aa0fed47aba7ecf0e86979706b1563eb7cb989
Prince <workwithprince2022@gmail.com>
2022-12-21 18:10:42 +0530

    added scroll in login and signup comps below headings

--- src/components/Login.js
+++ src/components/Login.js
@@ -69,21 +69,23 @@ const Login = () => {
     <div style={{ position: "fixed", top: "9.5vh", width: "100%", height: "90vh" }}>
       <div className='signup'>
         <h4 className='text-center mt-5 mb-4 bolder'>LOG IN</h4>
-        <p className='p-2 ps-4 rounded-3 mx-auto' style={alertStyle}>{loginRes}</p>
-        <div className='p-3 d-flex justify-content-center align-items-center row'>
-          <form className='signupform col-xl-4 col-lg-5 col-md-6 col-sm-8 col-10'>
-            <label htmlFor="emailInput">Email Address</label>
-            <input value={email} onChange={(e) => { setEmail(e.target.value.toLowerCase()) }} type="email" id="emailInput" placeholder='Enter Your Email Address' />
-            <label htmlFor="passInput">Password</label>
-            <input value={password} onChange={(e) => { setPassword(e.target.value) }} type="password" id="passInput" placeholder='Enter Your Password' />
-            <button onClick={login} type='submit' className='p-1 py-2 fw-bold fs-5 rounded-3'>LOG IN</button>
-          </form>
-        </div>
-        <center>
-          <div className="formLast d-flex justify-content-center col-xl-4 col-lg-5 col-md-6 col-sm-8 col-10">
-            <p className='me-2'>Already have an account? </p> <Link to='/signup'>Sign Up</Link>
+        <div className="signupScroll">
+          <p className='p-2 ps-4 rounded-3 mx-auto' style={alertStyle}>{loginRes}</p>
+          <div className='p-3 d-flex justify-content-center align-items-center row'>
+            <form className='signupform col-xl-4 col-lg-5 col-md-6 col-sm-8 col-10'>
+              <label htmlFor="emailInput">Email Address</label>
+              <input value={email} onChange={(e) => { setEmail(e.target.value.toLowerCase()) }} type="email" id="emailInput" placeholder='Enter Your Email Address' />
+              <label htmlFor="passInput">Password</label>
+              <input value={password} onChange={(e) => { setPassword(e.target.value) }} type="password" id="passInput" placeholder='Enter Your Password' />
+              <button onClick={login} type='submit' className='p-1 py-2 fw-bold fs-5 rounded-3'>LOG IN</button>
+            </form>
           </div>
-        </center>
+          <center>
+            <div className="formLast d-flex justify-content-center col-xl-4 col-lg-5 col-md-6 col-sm-8 col-10">
+              <p className='me-2'>Already have an account? </p> <Link to='/signup'>Sign Up</Link>
+            </div>
+          </center>
+        </div>
       </div>
     </div>
   )
